90c4e5c52e8918b52cde22be504a1ce8cea1f572
huyneeee <nqh.one.of.a.kind@gmail.com>
2024-04-08 09:30:00 +0700

    feat: add cache image

--- config/environment.ts
+++ config/environment.ts
@@ -1,3 +1,4 @@
 const LINK_SPLINE_MODEL = process.env.NEXT_PUBLIC_SPLINE_MODEL_URL || "";
+const APP_URL = process.env.NEXT_PUBLIC_LANDING_URL || "";
 
-export { LINK_SPLINE_MODEL };
+export { LINK_SPLINE_MODEL, APP_URL };
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate"
+          }
+        ]
+      }
+    ];
+  },
   output: 'standalone',
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default nextConfig;
